6a8d1beb0c55e4288b9dd47fd0e96850553e935d
KrevTos <166351812+KrevTos@users.noreply.github.com>
2025-04-02 21:41:18 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], 
   optimizeDeps: {
     exclude: ['lucide-react'],
     esbuildOptions: {
